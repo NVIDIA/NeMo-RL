c12aa60e06cb3b38166b60390ebd66f476a7c9e7
Yuki Huang <yukih@nvidia.com>
2025-03-25 06:49:42 +0000

    add evaluation implement and minial doc
    Signed-off-by: Yuki Huang <yukih@nvidia.com>

---
+++ nemo_reinforcer/evals/eval.py
@@ -0,0 +1,173 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from typing import Tuple, TypedDict
+
+import ray
+from torch.utils.data import DataLoader
+
+from nemo_reinforcer.data import DataConfig
+from nemo_reinforcer.data.datasets import AllTaskProcessedDataset, eval_collate_fn
+from nemo_reinforcer.data.llm_message_utils import get_keys_from_message_log
+from nemo_reinforcer.distributed.batched_data_dict import BatchedDataDict
+from nemo_reinforcer.distributed.virtual_cluster import ClusterConfig, RayVirtualCluster
+from nemo_reinforcer.environments.math_environment import MathEnvConfig
+from nemo_reinforcer.models.generation.interfaces import GenerationConfig
+from nemo_reinforcer.models.generation.vllm import VllmGeneration
+
+
+# ===============================================================================
+# Configuration
+# ===============================================================================
+
+
+class MasterConfig(TypedDict):
+    generate: GenerationConfig
+    data: DataConfig
+    math_env: MathEnvConfig
+    cluster: ClusterConfig
+
+
+# ===============================================================================
+# Setup & Initialization
+# ===============================================================================
+
+
+def setup(
+    master_config: MasterConfig,
+    dataset: AllTaskProcessedDataset,
+) -> Tuple[
+    VllmGeneration,
+    DataLoader,
+    MasterConfig,
+]:
+    """Set up components for model evaluation.
+
+    Initializes the VLLM model and data loader.
+
+    Args:
+        master_config: Configuration settings.
+        dataset: Dataset to evaluate on.
+
+    Returns:
+        VLLM model, data loader, and config.
+    """
+    # Extract individual configs for easier access
+    generation_config = master_config["generation"]
+    cluster_config = master_config["cluster"]
+
+    # ==========================
+    #           Data
+    # ==========================
+    if generation_config["num_prompts_per_step"] == -1:
+        generation_config["num_prompts_per_step"] = len(dataset)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=generation_config["num_prompts_per_step"],
+        shuffle=False,
+        collate_fn=eval_collate_fn,
+    )
+    print(f"  ✓ Evaluation dataset loaded with {len(dataset)} samples")
+
+    # ==========================
+    #          Cluster
+    # ==========================
+    print("\n▶ Setting up compute cluster...")
+    cluster = RayVirtualCluster(
+        name="eval_cluster",
+        bundle_ct_per_node_list=[cluster_config["gpus_per_node"]]
+        * cluster_config["num_nodes"],
+        use_gpus=True,
+        num_gpus_per_node=cluster_config["gpus_per_node"],
+        max_colocated_worker_groups=1,
+    )
+    print(f"  ✓ Ray cluster initialized with {cluster_config['num_nodes']} nodes")
+
+    # ==========================
+    #           Model
+    # ==========================
+    print("\n▶ Setting up model...")
+    backend = generation_config["backend"]
+    assert backend == "vllm", "Only vLLM backend is supported for evaluation"
+    vllm_generation = VllmGeneration(cluster=cluster, config=generation_config)
+    print(
+        f"  ✓ Using vLLM backend for generation with {generation_config['model_name']}"
+    )
+
+    print("\n" + "=" * 60)
+    print(" " * 18 + "SETUP COMPLETE")
+    print("=" * 60 + "\n")
+
+    return (
+        vllm_generation,
+        dataloader,
+        master_config,
+    )
+
+
+# ===============================================================================
+# Evaluation
+# ===============================================================================
+
+
+def run_env_eval(vllm_generation, dataloader, env, master_config):
+    """Main entry point for running evaluation using environment.
+
+    Generates model responses and evaluates them by env.
+
+    Args:
+        vllm_generation: Model for generating responses.
+        dataloader: Data loader with evaluation samples.
+        env: Environment that scores responses.
+        master_config: Configuration settings.
+    """
+    # Run evaluation loop
+    score, count = 0.0, 0
+    for batch in dataloader:
+        inputs = BatchedDataDict({"prompts": batch["prompts"]})
+        outputs = vllm_generation.generate_text(inputs)["texts"]
+
+        # append to message_log
+        for idx, output in enumerate(outputs):
+            batch["message_log"][idx].append(
+                {
+                    "role": "assistant",
+                    "content": output,
+                }
+            )
+
+        # evaluate generations with the environment
+        to_env = [
+            get_keys_from_message_log(batch["message_log"][i], ["role", "content"])
+            for i in range(len(batch["message_log"]))
+        ]
+        _, _, rewards, _ = ray.get(env.step.remote(to_env, batch["extra_env_info"]))
+
+        score += rewards.sum().item()
+        count += len(rewards)
+
+    # Cleanup before printing results
+    ray.get(env.shutdown.remote())
+    vllm_generation.shutdown()
+
+    # Print results
+    dataset_name = os.path.basename(master_config["data"]["dataset_name"])
+    model_name = os.path.basename(master_config["generation"]["model_name"])
+    average_score = score / count
+
+    print("\n" + "=" * 60)
+    print(f"{model_name=} {dataset_name=}")
+    print(f"score={average_score:.2f} ({score}/{count})")
+    print("=" * 60 + "\n")
