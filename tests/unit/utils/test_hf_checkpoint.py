7eee5689e14948d9a6555cb0d3b5b7db60435461
ashors1 <ashors@nvidia.com>
2025-04-02 17:49:12 -0700

    add HF checkpoint save test
    Signed-off-by: ashors1 <ashors@nvidia.com>

--- tests/unit/utils/test_hf_checkpoint.py
+++ tests/unit/utils/test_hf_checkpoint.py
@@ -16,12 +16,27 @@ import os
 import pytest
 import torch
 
+from nemo_reinforcer.distributed.batched_data_dict import BatchedDataDict
+from nemo_reinforcer.distributed.virtual_cluster import RayVirtualCluster
+from nemo_reinforcer.models.policy.hf_policy import HfPolicy
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from nemo_reinforcer.utils.hf_checkpoint import (
     load_checkpoint,
     save_checkpoint,
     ModelState,
     OptimizerState,
 )
+
+# Define basic test config
+simple_policy_config = {
+    "model_name": "meta-llama/Llama-3.2-1B",  # "hf-internal-testing/tiny-random-Gemma3ForCausalLM",
+    "tokenizer_name": "meta-llama/Llama-3.2-1B",  # "hf-internal-testing/tiny-random-Gemma3ForCausalLM",
+    "train_global_batch_size": 32,
+    "train_micro_batch_size": 1,
+    "logprob_batch_size": 1,
+    "max_total_sequence_length": 1024,
+    "precision": "float32",
+}
 
 
 @pytest.fixture
@@ -39,6 +54,43 @@ def test_experiment():
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
 
     return model, optimizer, scheduler
+
+
+## TODO: check scope
+@pytest.fixture(scope="module")
+def cluster():
+    """Create a virtual cluster for testing."""
+    # Create a cluster with 1 GPU
+    virtual_cluster = RayVirtualCluster(
+        bundle_ct_per_node_list=[1],  # [2],  # 1 node with 2 GPU bundle
+        use_gpus=True,
+        max_colocated_worker_groups=1,  # 2,
+        num_gpus_per_node=1,  # 2,  # Use available GPUs
+        name="test-cluster",
+    )
+    yield virtual_cluster
+    virtual_cluster.shutdown()
+
+
+@pytest.fixture(scope="function")
+def tokenizer():
+    """Initialize tokenizer for the test model."""
+    tokenizer_name = simple_policy_config["tokenizer_name"]
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, trust_remote_code=True)
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+    return tokenizer
+
+
+@pytest.fixture(scope="function")
+def policy(cluster, tokenizer):
+    """Initialize the policy."""
+    return HfPolicy(
+        cluster=cluster,
+        config=simple_policy_config,
+        init_optimizer=False,
+        init_reference_model=False,
+    )
 
 
 ## recursively get the dummy state dict
@@ -122,7 +174,7 @@ def test_save_and_load_model_only(test_experiment):
     save_checkpoint(test_model, "/tmp/test_model_only")
     assert os.path.exists("/tmp/test_model_only")
     assert not os.path.exists("/tmp/test_model_only-hf")
-    assert os.listdir("/tmp/test_model_only") == [".metadata", "__0_0.distcp"]
+    assert set(os.listdir("/tmp/test_model_only")) == {".metadata", "__0_0.distcp"}
 
 
 def test_save_and_load_model_and_optimizer(test_experiment):
@@ -138,13 +190,14 @@ def test_save_and_load_model_and_optimizer(test_experiment):
         optimizer_path="/tmp/model_and_optimizer/optimizer",
     )
 
-    assert os.path.exists("/tmp/model_and_optimizer/model")
-    assert os.path.exists("/tmp/model_and_optimizer/optimizer")
-    assert os.listdir("/tmp/model_and_optimizer/model") == [".metadata", "__0_0.distcp"]
-    assert os.listdir("/tmp/model_and_optimizer/optimizer") == [
+    assert set(os.listdir("/tmp/model_and_optimizer/model")) == {
         ".metadata",
         "__0_0.distcp",
-    ]
+    }
+    assert set(os.listdir("/tmp/model_and_optimizer/optimizer")) == {
+        ".metadata",
+        "__0_0.distcp",
+    }
 
     ## modify the model, optimizer, and scheduler and verify that loading the checkpoint overrides the values
     new_linear = torch.nn.Linear(4, 4)
@@ -174,3 +227,47 @@ def test_save_and_load_model_and_optimizer(test_experiment):
     assert scheduler.state_dict() == new_scheduler.state_dict()
     check_dict_equality(new_model.state_dict(), test_model.state_dict())
     check_dict_equality(new_optimizer.state_dict(), optimizer.state_dict())
+
+
+def test_save_and_load_hf_checkpoint(policy):
+    ## warm up with a forward pass
+    ## this is needed before saving a checkpoint because FSDP does some lazy initialization
+    input_ids = torch.randint(0, 16000, (4, 128))  # 4 sequences, each of length 128
+    attention_mask = torch.ones(4, 128)
+    input_lengths = attention_mask.sum(dim=1).to(torch.int32)
+    dummy_fwd_dict = BatchedDataDict(
+        {
+            "input_ids": input_ids,
+            "input_lengths": input_lengths,
+            "attention_mask": attention_mask,
+            "labels": torch.randint(0, 16000, (4, 128)),
+        }
+    )
+    policy.get_logprobs(dummy_fwd_dict)
+
+    policy.save_checkpoint(
+        "/tmp/test_hf_and_dcp",
+        save_hf=True,
+        save_torch_dist=True,
+    )
+
+    ## make sure we save both HF and DCP checkpoints
+    assert set(os.listdir("/tmp/test_hf_and_dcp")) == {"__0_0.distcp", ".metadata"}
+    ## 1B model has two shards
+    assert set(os.listdir("/tmp/test_hf_and_dcp-hf")) == {
+        "config.json",
+        "generation_config.json",
+        "model-00001-of-00002.safetensors",
+        "model-00002-of-00002.safetensors",
+        "model.safetensors.index.json",
+    }
+
+    coverted_model = AutoModelForCausalLM.from_pretrained("/tmp/test_hf_and_dcp-hf")
+    original_model = AutoModelForCausalLM.from_pretrained(
+        simple_policy_config["model_name"]
+    )
+
+    ## make sure this model matches the original
+    check_dict_equality(coverted_model.state_dict(), original_model.state_dict())
+
+    policy.worker_group.shutdown()
