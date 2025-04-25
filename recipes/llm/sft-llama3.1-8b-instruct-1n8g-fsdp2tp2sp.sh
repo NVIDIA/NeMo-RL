f620160787b2a07dcc8bc01b548a6a677225f943
Terry Kong <terryk@nvidia.com>
2025-04-25 00:32:59 -0700

    finalized sft-llama3.1-8b-instruct-1n8g-fsdp2tp2sp Signed-off-by: Terry Kong <terryk@nvidia.com>

--- recipes/llm/sft-llama3.1-8b-instruct-1n8g-fsdp2tp2sp.sh
+++ recipes/llm/sft-llama3.1-8b-instruct-1n8g-fsdp2tp2sp.sh
@@ -58,11 +58,13 @@ python -u examples/run_sft.py \
 # Convert tensorboard logs to json
 python -u tests/json_dump_tb_logs.py $LOG_DIR --output_path $JSON_METRICS
 
+# TODO: memory check will fail due to OOM tracked here https://github.com/NVIDIA/reinforcer/issues/263
+
 # Only run metrics if the target step is reached
 if [[ $(jq 'to_entries | .[] | select(.key == "train/loss") | .value | keys | map(tonumber) | max' $JSON_METRICS) -ge $MAX_STEPS ]]; then
     # TODO: FIGURE OUT CORRECT METRICS
     python -u tests/check_metrics.py $JSON_METRICS \
-        'data["train/loss"]["1"] < 2.4' \
-        'data["train/loss"]["60"] < 0.45' \
-        'max(data["ray/node.0.gpu.0.memory"]) < 30000'
+        'data["train/loss"]["1"] < 5' \
+        'data["train/loss"]["60"] < 0.5' \
+        'max(data["ray/node.0.gpu.0.memory"]) < 45000'
 fi 
