33b106cf40c9ef1e3bec972404f549b986a7d490
Parth Chadha <pchadha@nvidia.com>
2025-04-28 12:54:41 -0700

    Update nemo_rl/models/policy/dtensor_policy_worker.py
    Co-authored-by: Sahil Jain <48468750+SahilJain314@users.noreply.github.com>
    Signed-off-by: Parth Chadha <parth29@gmail.com>
    Signed-off-by: Parth Chadha <pchadha@nvidia.com>

--- nemo_rl/models/policy/dtensor_policy_worker.py
+++ nemo_rl/models/policy/dtensor_policy_worker.py
@@ -142,6 +142,7 @@ class DTensorPolicyWorker:
             device_map="cpu",  # load weights onto CPU initially
             torch_dtype=torch.float32,  # use full precision in sft until https://github.com/NVIDIA/nemo-rl/issues/13 is fixed
         )
+        # caching since this property is not always preserved after FSDP
         self.num_tied_weights = len(find_tied_parameters(self.model))
 
         self.tokenizer = tokenizer
