983a2b2d168e5028d87467225ba805f7acb1098c
ashors1 <ashors@nvidia.com>
2025-04-28 12:19:03 -0700

    remove online hf checkpointing
    Signed-off-by: ashors1 <ashors@nvidia.com>

--- nemo_reinforcer/algorithms/grpo.py
+++ nemo_reinforcer/algorithms/grpo.py
@@ -532,12 +532,6 @@ def grpo_train(
             ):  # +1 because step is 0-indexed
                 policy.prepare_for_training()
 
-                is_last_checkpoint = (
-                    min(len(dataloader), master_config["grpo"]["max_num_steps"])
-                    - (step + 1)
-                    < master_config["checkpointing"]["save_period"]
-                )
-
                 grpo_save_state["step"] = step + 1
                 grpo_save_state["val_reward"] = val_metrics["accuracy"]
                 grpo_save_state["consumed_samples"] = consumed_samples
@@ -554,7 +548,6 @@ def grpo_train(
                         tokenizer_path=os.path.join(
                             checkpoint_path, "policy", "tokenizer"
                         ),
-                        save_hf=is_last_checkpoint,
                     )
                     torch.save(
                         dataloader.state_dict(),
