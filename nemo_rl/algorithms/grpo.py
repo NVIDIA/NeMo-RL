1f9eb34f10197ddc14303c7d554aff123ede2ff6
Zhiyu Li <zhiyul@nvidia.com>
2025-05-08 00:01:07 +0000

    token_mult_prob_error sample visualization
    Signed-off-by: Zhiyu Li <zhiyul@nvidia.com>
    
    new
    
    Signed-off-by: Zhiyu Li <zhiyul@nvidia.com>
    
    fix
    
    Signed-off-by: Zhiyu Li <zhiyul@nvidia.com>

--- nemo_rl/algorithms/grpo.py
+++ nemo_rl/algorithms/grpo.py
@@ -585,7 +585,6 @@ def grpo_train(
         log_data["input_lengths"] = input_lengths.tolist()
         logger.log_batched_dict_as_jsonl(log_data, f"train_data_step{step}.jsonl")
 
-        print("\n📊 Training Results:")
         metrics = {
             "loss": train_results["loss"].numpy(),
             "reward": rewards.numpy(),
@@ -600,6 +599,21 @@ def grpo_train(
         metrics.update(rollout_metrics)
 
         timing_metrics: dict[str, float] = timer.get_timing_metrics(reduction_op="sum")  # type: ignore
+        if metrics["token_mult_prob_error"] > (token_mult_prob_error_threshold := 1.05):
+            logger.log_plot_token_mult_prob_error(
+                {
+                    "prompt_lengths": repeated_batch["length"],
+                    "full_lengths": input_lengths,
+                    "generation_logprobs": train_data["generation_logprobs"],
+                    "prev_logprobs": train_data["prev_logprobs"],
+                    "token_mask": train_data["token_mask"],
+                    "sample_mask": train_data["sample_mask"],
+                },
+                step,
+                name="train/token_mult_prob_error_plot_sample",
+            )
+
+        print("\n📊 Training Results:")
 
         print(f"  • Loss: {metrics['loss']:.4f}")
         print(f"  • Avg Reward: {np.mean(rewards.numpy()):.4f}")
